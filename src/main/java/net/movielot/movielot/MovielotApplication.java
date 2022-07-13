2e1417b7f4230804fb77576d26823ca84bd9a591
oopty <tiramisu13795@gmail.com>
2022-07-13 21:40:51 +0900

    :tada: init commit!!

---
+++ src/main/java/net/movielot/movielot/MovielotApplication.java
@@ -0,0 +1,13 @@
+package net.movielot.movielot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/movielot/movielot/MovielotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.movielot.movielot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
