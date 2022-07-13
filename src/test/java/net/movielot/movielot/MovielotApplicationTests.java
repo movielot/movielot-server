4a9055fe60ef64a0d906e5adfb375e1d804fd19a
oopty <tiramisu13795@gmail.com>
2022-07-13 23:11:52 +0900

    github action test

--- src/test/java/net/movielot/movielot/MovielotApplicationTests.java
+++ src/test/java/net/movielot/movielot/MovielotApplicationTests.java
@@ -3,11 +3,14 @@ package net.movielot.movielot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class MovielotApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isFalse();
 	}
 
 }
