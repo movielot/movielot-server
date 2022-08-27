7f1e141ffc51fff264649ce06e19f40b5ddadc70
oopty <tiramisu13795@gmail.com>
2022-08-27 20:54:44 +0900

    Modified API for Movie Details
    - 평점 정보 json property name 수정

--- src/main/java/net/movielot/movielot/response/imdb/IMDBRatingResponse.java
+++ src/main/java/net/movielot/movielot/response/imdb/IMDBRatingResponse.java
@@ -1,13 +1,12 @@
 package net.movielot.movielot.response.imdb;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.movielot.movielot.domain.Movie;
 
 @Data
 @AllArgsConstructor
@@ -15,12 +14,14 @@ import net.movielot.movielot.domain.Movie;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class IMDBRatingResponse {
-    private String imDbId;
+    @JsonProperty("imDbId")
+    private String imdbId;
     private String title;
     private String fullTitle;
     private String type;
     private String year;
-    private String imDb;
+    @JsonProperty("imDb")
+    private String imdb;
     private String metacritic;
     private String theMovieDb;
     private String rottenTomatoes;
